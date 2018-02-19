f471f79561f73c66021df0b65d2eacfc0ec6a6b3
s-a-w <w.shubham2594@gmail.com>
2018-02-19 14:58:58 +0530

    added favicon and video

--- js/application.js
+++ js/application.js
@@ -1,8 +1,17 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
-	$(".toggle-icon").click(function() {
+	$(".toggle-icon").click(function () {
   	$(this).toggleClass("active");
   	$(".side-nav").toggleClass("shrink");
+	});
+
+	$(".profile-wrap").click(function (event) {
+		event.stopPropagation();
+		$(".user-dropDown").toggleClass("hide");
+	});
+
+	$(document).click(function() {
+	    $('.user-dropDown').addClass('hide');
 	});
 
 });
